ef0efea2681522af354777dc27b0a50b4eb3fe5f
GustavoDavidC <gustavodavidc2014@gmail.com>
2021-05-27 12:19:15 -0300

    Se perfecciona formulario

--- js/forumlario.js
+++ js/forumlario.js
@@ -1,37 +1,38 @@
 const formulario = document.getElementById("formulario");
-var entradas = document.getElementsByClassName("entrada");
-var vacio = document.getElementById("esta-vacio");
-var avisos = document.getElementsByTagName("p");
-var nombre= /^[a-zÑñA-ZÀ-ÿ\s]{2,40}$/, 
+const vacio = document.getElementById("esta-vacio");
+const avisos = document.getElementsByTagName("p");
+const nombre= /^[a-zÑñA-ZÀ-ÿ\s]{2,40}$/, 
     apellido= /^[a-zÑñA-ZÀ-ÿ\s]{2,40}$/,
 	correo= /^[a-zñÑA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono= /^\d{3,20}$/ ;
-var validaciones = [nombre, apellido, correo, telefono];
+const validaciones = [nombre, apellido, correo, telefono];
 
-function validar(){
-   for(var i=0; i<5; i++){
-       if(entradas[i].value==0){
-           vacio.classList.remove("esta-vacio");
-           vacio.classList.add("esta-vacio-si");
-           return false;
-       }
-       else{
-        vacio.classList.add("esta-vacio");
-        vacio.classList.remove("esta-vacio-si");
-       }
-   }
+formulario.addEventListener("submit", (e)=>{
+    let verificacion =0;
+    e.preventDefault();
+    for( i=0; i<5; i++){
+        if(formulario.getElementsByClassName("entrada")[i].value==0){
+            vacio.classList.replace("esta-vacio", "esta-vacio-si");
+            verificacion=1;
+        }
+        else{
+         vacio.classList.replace("esta-vacio-si", "esta-vacio");
+        }
+    }
+ 
+    for( i=0; i<4; i++){
+        if(!(validaciones[i].test(formulario.getElementsByClassName("entrada")[i].value))){
+             avisos[i].classList.replace("aviso", "aviso-ok");
+             verificacion=1;
+        }else{
+         avisos[i].classList.replace("aviso-ok", "aviso");
+        }
+        
+    }
 
-   for(var i=0; i<4; i++){
-       if(!(validaciones[i].test(entradas[i].value))){
-            avisos[i].classList.remove("aviso");
-            avisos[i].classList.add("aviso-ok");
-            return(false);
-       }else{
-        avisos[i].classList.add("aviso");
-        avisos[i].classList.remove("aviso-ok");
-       }
-       
-   }
-   alert("formulario enviado con exito");
-   formulario.submit();
-}
+    if(verificacion==0){
+        alert("formulario enviado!");
+        formulario.submit();
+    }
+})
+   
